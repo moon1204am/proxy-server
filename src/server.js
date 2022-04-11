a705807b086bf8e6d129435c1650482e5578df0d
Maya <nabi@MacBook-Air.local>
2022-04-11 15:35:36 +0200

    initial commit

---
+++ src/server.js
@@ -0,0 +1,39 @@
+'use strict';
+
+const path = require('path');
+const APP_ROOT_DIR = path.join(__dirname, '..');
+
+const result = require('dotenv-safe').config({
+    path: path.join(APP_ROOT_DIR, '.env'),
+    example: path.join(APP_ROOT_DIR, '.env.example')
+});
+
+const express = require('express');
+const app = express();
+
+const axios = require('axios');
+const instance = axios.create({
+    baseURL: 'https://api.playground.klarna.com',
+    headers: {
+        'Content-type': "application/json",
+        'Authorization': `Basic ${process.env.CREDENTIALS}`
+    }
+})
+
+const cors = require('cors');
+app.use(cors());
+
+app.post('/order', async (req, res) => {
+    const orderDetails = req.body;
+    const response = await instance.post("/checkout/v3/orders/", orderDetails);
+    res.json(await response.data);
+})
+
+app.get('/order/:order_id', async (req, res) => {
+    const response = await instance.get("/checkout/v3/orders/" + req.params.order_id);
+    res.json(await response.data);
+})
+
+app.listen(process.env.SERVER_PORT, () => {
+    console.log("Listening on port " + process.env.SERVER_PORT);
+})
